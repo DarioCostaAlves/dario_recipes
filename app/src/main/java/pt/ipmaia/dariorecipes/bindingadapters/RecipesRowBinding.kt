81e1a0924aa9484dd4096bb3896667f1d2a4aa4b
DarioCostaAlves <dario.c.alves22@gmail.com>
2023-01-26 22:35:53 +0000

    recipes details

--- app/src/main/java/pt/ipmaia/dariorecipes/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/pt/ipmaia/dariorecipes/bindingadapters/RecipesRowBinding.kt
@@ -1,16 +1,36 @@
 package pt.ipmaia.dariorecipes.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import pt.ipmaia.dariorecipes.R
+import pt.ipmaia.dariorecipes.models.Result
+import pt.ipmaia.dariorecipes.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesRowBinding {
 
     companion object {
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            Log.d("onRecipeClickListener", "TESTECLICK")
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
